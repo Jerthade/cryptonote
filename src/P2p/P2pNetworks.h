70366010a0892b80327c243f2885ddfe8c0df3c6
Jerthade <45024187+Jerthade@users.noreply.github.com>
2019-03-06 22:38:41 -0500

    Update P2pNetworks.h

--- src/P2p/P2pNetworks.h
+++ src/P2p/P2pNetworks.h
@@ -6,5 +6,5 @@
 
 namespace CryptoNote
 {
-  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { a522, 2367, e1ab, 9a1d, bfeb, 248f, 81bb, c922, 4f25, 85ef, e69a, c2be, db9f, 2b98, f73c, 69bd } };
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 1522, 2367, 11ab, 1a1d, 1feb, 2481, 8111, 1922, 1f25, 15ef, 169a, 12be, 1b9f, 2b98, 173c, 69bd } };
 }
